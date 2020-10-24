e241de7c4bf3e52be585daefadb9f5c616296fc3
alana_eloeva <alana.eloeva@bk.ru>
2020-10-24 20:24:43 +0300

    google geocoder, yandex geocoder

---
+++ app/src/main/java/com/postoffice/model/yandex/YaResponseModel.java
@@ -0,0 +1,14 @@
+package com.postoffice.model.yandex;
+
+public class YaResponseModel {
+
+    private YaResponse response;
+
+    public YaResponse getResponse() {
+        return response;
+    }
+
+    public void setResponse(YaResponse response) {
+        this.response = response;
+    }
+}
