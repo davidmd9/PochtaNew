40ff9647e9a85b8f9167cf41ad21c75a30828759
batraz_0 <batraz_0@hotmail.com>
2020-10-24 15:34:21 +0300

    + add fragment

---
+++ app/src/main/java/com/postoffice/fragments/AddressCorrectionAdditionalFragment.java
@@ -0,0 +1,14 @@
+package com.postoffice.fragments;
+
+import android.view.View;
+
+import com.postoffice.base.BaseFragment;
+
+public class AddressCorrectionAdditionalFragment extends BaseFragment {
+
+
+    @Override
+    protected void bindView(View view) {
+
+    }
+}
