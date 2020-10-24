cc3a0ab1e5785445902957cd8761c0c55809b7ce
David Minasyan <davidmd@yandex.ru>
2020-10-24 20:45:06 +0300

    Added map

--- app/src/main/java/com/postoffice/fragments/TaskAdressFragment.java
+++ app/src/main/java/com/postoffice/fragments/TaskAdressFragment.java
@@ -2,6 +2,7 @@ package com.postoffice.fragments;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,13 +10,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.postoffice.R;
+import com.postoffice.base.BaseFragment;
 
 /**
  * A simple {@link Fragment} subclass.
  * create an instance of this fragment.
  */
-public class TaskAdressFragment extends Fragment {
+public class TaskAdressFragment extends BaseFragment implements OnMapReadyCallback {
 
 
     public TaskAdressFragment() {
@@ -33,6 +37,22 @@ public class TaskAdressFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task_adress, container, false);
+        View v =  inflater.inflate(R.layout.fragment_task_adress, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        return v;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+
+    @Override
+    protected void bindView(View view) {
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        setToolbar(toolbar, "");
+
     }
 }
